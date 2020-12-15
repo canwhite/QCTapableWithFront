42cd994d399b61da951bf207f50ebe30fef3b601
qiaochao <qiaochao@csii.com.cn>
2020-12-15 17:46:00 +0800

    改好的tapable包和在vue中使用

---
+++ vue-tapable/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import {SyncHook} from './libs/tapable'
+export default {
+  name: 'App',
+  created(){
+
+    let hook = new SyncHook(['name']);
+    hook.tap('1',(name)=>{
+      console.log('-----',name);
+    })
+    hook.call('zack')
+
+
+  }
+
+
+}
+
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-tapable/src/libs/tapable/index.js
@@ -0,0 +1,52 @@
+/*
+	MIT License http://www.opensource.org/licenses/mit-license.php
+	Author Tobias Koppers @sokra
+*/
+"use strict";
+
+
+
+// exports.__esModule = true;
+// exports.SyncHook = require("./SyncHook");
+// exports.SyncBailHook = require("./SyncBailHook");
+// exports.SyncWaterfallHook = require("./SyncWaterfallHook");
+// exports.SyncLoopHook = require("./SyncLoopHook");
+// exports.AsyncParallelHook = require("./AsyncParallelHook");
+// exports.AsyncParallelBailHook = require("./AsyncParallelBailHook");
+// exports.AsyncSeriesHook = require("./AsyncSeriesHook");
+// exports.AsyncSeriesBailHook = require("./AsyncSeriesBailHook");
+// exports.AsyncSeriesLoopHook = require("./AsyncSeriesLoopHook");
+// exports.AsyncSeriesWaterfallHook = require("./AsyncSeriesWaterfallHook");
+// exports.HookMap = require("./HookMap");
+// exports.MultiHook = require("./MultiHook");
+
+
+import SyncHook from './SyncHook';
+import SyncBailHook from './SyncBailHook';
+import SyncWaterfallHook from './SyncWaterfallHook';
+import SyncLoopHook from './SyncLoopHook';
+import AsyncParallelHook from './AsyncParallelHook';
+import AsyncParallelBailHook from  './AsyncParallelBailHook';
+import AsyncSeriesHook from './AsyncSeriesHook';
+import AsyncSeriesBailHook from './AsyncParallelBailHook';
+import AsyncSeriesLoopHook from './AsyncSeriesLoopHook';
+import AsyncSeriesWaterfallHook from './AsyncSeriesWaterfallHook';
+import HookMap from './HookMap';
+import MultiHook from './MultiHook';
+
+
+export {
+	SyncHook,
+	SyncBailHook,
+	SyncWaterfallHook,
+	SyncLoopHook,
+	AsyncParallelHook,
+	AsyncParallelBailHook,
+	AsyncSeriesHook,
+	AsyncSeriesBailHook,
+	AsyncSeriesLoopHook,
+	AsyncSeriesWaterfallHook,
+	HookMap,
+	MultiHook
+
+};
